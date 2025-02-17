054510d17a3607d4b9675eb07497f8c74ce9d718
hossamragab97 <hossamragab97>
2025-02-17 20:54:26 +0200

    movie search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,85 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { TableModule } from 'primeng/table';
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';  
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { DatePickerModule } from 'primeng/datepicker';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ToastModule } from 'primeng/toast';
+import { SpeedDialModule } from 'primeng/speeddial';
+import { MessageService } from 'primeng/api';
+import { ScrollTopModule } from 'primeng/scrolltop';
+import {  HttpClientModule } from '@angular/common/http';
+import { KnobModule } from 'primeng/knob';
+import { HomeComponent } from './home/home.component';
+
+import { AutoFocusModule } from 'primeng/autofocus';
+import { ToolbarModule } from 'primeng/toolbar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
+import { ImageModule } from 'primeng/image';
+import { PaginatorModule } from 'primeng/paginator';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
+    ReactiveFormsModule,
+    PaginatorModule,
+    ScrollPanelModule,
+    ImageModule,
+    CardModule,
+    InputTextModule,
+    KnobModule,
+    ToolbarModule,
+    AutoFocusModule,
+    HttpClientModule,
+    ToastModule,
+    ScrollTopModule,
+    InputGroupModule,
+    SpeedDialModule,
+    InputGroupAddonModule,
+    DatePickerModule,
+    ToggleButtonModule,
+    InputIconModule,
+    IconFieldModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule ,
+    ButtonModule ,
+    TableModule,
+    CalendarModule,
+    FormsModule,
+    BrowserAnimationsModule ,
   ],
-  providers: [],
+  providers: [
+    MessageService,
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+          preset: Aura,
+          options: {
+            darkModeSelector: '.my-app-dark',
+        }
+      }
+  })
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,90 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { FormControl } from '@angular/forms';
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  apiKey = '95a9fcd369b78a1647a4e494b721a944'
+  baseUrl = 'https://api.themoviedb.org/3/search/movie'
+  pathImg ='https://image.tmdb.org/t/p/w500'
+  noImg = 'https://media.istockphoto.com/id/1055079680/vector/black-linear-photo-camera-like-no-image-available.jpg?s=612x612&w=0&k=20&c=P1DebpeMIAtXj_ZbVsKVvg-duuL0v9DlrOZUvPG6UJk='
+
+  isLight = true
+  searchControl = new FormControl();
+  searchText: string = '';
+  movies: any = [];
+  totalRecords = 0
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+
+    this.searchControl.valueChanges
+      .pipe(
+        debounceTime(500), 
+        distinctUntilChanged()
+      )
+      .subscribe(value => {
+        this.fetchMovies(value , 1);
+      });
+
+
+    let theme = localStorage.getItem('them') ?? 'dark'
+    const element = document.querySelector('html') as HTMLElement;
+    if (theme == 'light') {
+      this.isLight = true
+      element.classList.remove('my-app-dark');
+    } else {
+      this.isLight = false
+      element.classList.add('my-app-dark');
+    }
+
+
+    this.searchText = this.getRandomQuery(); 
+    this.fetchMovies(this.searchText, 1);
+
+  }
+
+
+  getRandomQuery(): string {
+    const words = ['Avengers', 'Batman', 'Spiderman', 'Matrix', 'Joker', 'Titanic', 'Inception', 'Alien'];
+    return words[Math.floor(Math.random() * words.length)];
+  }
+
+  fetchMovies(query: string, page: number) {
+    this.http.get(`${this.baseUrl}?api_key=${this.apiKey}&query=${query}&page=${page}`)
+      .subscribe((response: any) => {
+        this.movies = response.results;
+        this.movies.forEach((movie: any) => {
+          movie.vote_average = Number(movie.vote_average.toFixed(1));
+        });
+        this.totalRecords = response.total_results
+        document.getElementById('movies')?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      });
+  }
+
+  onPageChange(event: any) {
+    this.fetchMovies(this.searchText, event.page + 1)
+  }
+
+
+  toggleDarkMode() {
+    if (!localStorage.getItem('them') || localStorage.getItem('them') == 'light') {
+      this.isLight = false
+      localStorage.setItem('them', 'dark')
+    } else {
+      this.isLight = true
+      localStorage.setItem('them', 'light')
+    }
+    const element = document.querySelector('html') as HTMLElement;
+    element.classList.toggle('my-app-dark');
+  }
+
+
+}
